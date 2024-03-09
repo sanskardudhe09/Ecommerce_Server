75405c73a7fcd52131485228b2370a466b4065d5
sanskardudhe09 <sanskardudhe2018@gmail.com>
2024-03-10 00:14:41 +0530

    modified

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const categoryRoute = require('./routes/categoryRoute.js');
 const productRoute = require('./routes/productRoute.js')
 const morgan = require('morgan');
 const path = require('path');
+var cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
 conn();
