0c03641b89ede081edab3663837cc574924d1145
sanskardudhe09 <sanskardudhe2018@gmail.com>
2024-03-09 23:18:31 +0530

    modified

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ conn();
 app.use("/api", authRoute);
 app.use("/api", categoryRoute);
 app.use("/api", productRoute);
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+ });
 const port = process.env.PORT || 8000;
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
